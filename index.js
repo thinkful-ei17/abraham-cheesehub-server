01e8c64926615a04f99ddc97c3e008599cc28d62
Abraham Cuenca <websiteprojectmanager@gmail.com>
2018-02-02 21:12:28 -0800

    Add the /api/cheeses endpoint

--- index.js
+++ index.js
@@ -10,30 +10,55 @@ const app = express();
 
 app.use(
     morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
-        skip: (req, res) => process.env.NODE_ENV === 'test'
+      skip: (req, res) => process.env.NODE_ENV === 'test'
     })
 );
 
 app.use(
     cors({
-        origin: CLIENT_ORIGIN
+      origin: CLIENT_ORIGIN
     })
 );
 
+app.get('/api/cheeses', (req, res) => {
+  res.json(
+    [
+      'Bath Blue',
+      'Barkham Blue',
+      'Buxton Blue',
+      'Cheshire Blue',
+      'Devon Blue',
+      'Dorset Blue Vinney',
+      'Dovedale',
+      'Exmoor Blue',
+      'Harbourne Blue',
+      'Lanark Blue',
+      'Lymeswold',
+      'Oxford Blue',
+      'Shropshire Blue',
+      'Stichelton',
+      'Stilton',
+      'Blue Wensleydale',
+      'Yorkshire Blue'
+    ]
+  );
+});
+
+
 function runServer(port = PORT) {
-    const server = app
+  const server = app
         .listen(port, () => {
-            console.info(`App listening on port ${server.address().port}`);
+          console.info(`App listening on port ${server.address().port}`);
         })
         .on('error', err => {
-            console.error('Express failed to start');
-            console.error(err);
+          console.error('Express failed to start');
+          console.error(err);
         });
 }
 
 if (require.main === module) {
-    dbConnect();
-    runServer();
+  dbConnect();
+  runServer();
 }
 
 module.exports = {app};
