a989c31fd09b41fd32a0e22405c0bc74d75f9322
Abraham Cuenca <websiteprojectmanager@gmail.com>
2018-02-03 20:47:03 -0800

    Add seed data based on original array

---
+++ cheeses/v1/seed-data.js
@@ -0,0 +1,17 @@
+{"cheese": "Bath Blue"}
+{"cheese": "Barkham Blue"}
+{"cheese": "Buxton Blue"}
+{"cheese": "Cheshire Blue"}
+{"cheese": "Devon Blue"}
+{"cheese": "Dorset Blue Vinney"}
+{"cheese": "Dovedale"}
+{"cheese": "Exmoor Blue"}
+{"cheese": "Harbourne Blue"}
+{"cheese": "Lanark Blue"}
+{"cheese": "Lymeswold"}
+{"cheese": "Oxford Blue"}
+{"cheese": "Shropshire Blue"}
+{"cheese": "Stichelton"}
+{"cheese": "Stilton"}
+{"cheese": "Blue Wensleydale"}
+{"cheese": "Yorkshire Blue"}
